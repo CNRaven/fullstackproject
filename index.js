265161edc2c20af5e1b22dae8a9782615e562013
SilviaPetruta <silvia_petruta@yahoo.com>
2020-06-29 15:00:51 +0100

    update the index.js: app.use(express.urlencoded(extended: true)); and so on..

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('.mongoose');
 const key = require('./config/keys');
 
+const app = express();
+
 mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -9,10 +11,11 @@ mongoose.connect(keys.mongoURI, {
     useUnifiedTopology: true
 });
 
-const app = express();
+app.use(express.urlencoded(extended: true));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({ someText: "Hello"});
+    res.send(200).json({ someText: "Hello"});
 });
 
 const PORT = process.env.PORT || 5000;
