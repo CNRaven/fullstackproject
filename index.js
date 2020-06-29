994f1f3a45e869d767fbb7d3e93a771141b22fdd
SilviaPetruta <silvia_petruta@yahoo.com>
2020-06-29 15:02:30 +0100

    update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(keys.mongoURI, {
     useUnifiedTopology: true
 });
 
-app.use(express.urlencoded(extended: true));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
